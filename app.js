26fb3ba7b8b198bf0cd0be0eaaccedfe573a0e09
{gurleen-kaur5} <{gurleennn5@gmail.com}>
2025-05-26 16:25:40 +0530

    added search button

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError= require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -23,7 +24,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js")
 
-const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+// const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl= process.env.ATLASDB_URL;
 
 
 main().then(()=>{
@@ -33,7 +35,7 @@ main().then(()=>{
 })
 
 async function main(){
-    await mongoose.connect(mongo_url);
+    await mongoose.connect(dbUrl);
 };
 
 app.set("view engine", "ejs");
@@ -44,7 +46,20 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto:{
+        secret: 'secretstring',
+    }, 
+    touchAfter: 24*3600 ,
+  })
+
+store.on("error" , ()=>{
+    console.log("Error in mongo session store", err);
+} )
+
 const  sessionOptions = {
+    store,
     secret: 'secretstring',
     resave: false,
     saveUninitialized: true,
@@ -56,9 +71,11 @@ const  sessionOptions = {
     }
   }
   
-app.get("/",(req,res)=>{
-    res.send("hi i am root");
-})
+// app.get("/",(req,res)=>{
+//     res.send("hi i am root");
+// })
+
+
 
 //session and flash should be mentioned before routes
 app.use(session(sessionOptions));
